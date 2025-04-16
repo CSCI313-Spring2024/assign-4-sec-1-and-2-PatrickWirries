cfbb4734345b24e57d8e4a4ea35080a325d24567
PatrickWirries <patrickwirries@outlook.com>
2025-04-15 19:27:07 -0500

    Added contacts, updaters, deletions

---
+++ src/app/data/contact-data.ts
@@ -0,0 +1,25 @@
+import { Contact } from "../contact";
+
+export const Contacts:Contact[] = [
+{
+    id:0,
+    firstName: "Bobby",
+    lastName: 'Brown' ,
+    phoneNumber: '218-791-2185',
+    email: 'BobbyBrown@yahoo.com'
+},
+{
+    id:1,
+    firstName: 'Joshua',
+    lastName: 'Greggy',
+    phoneNumber: '701-218-7911',
+    email: 'jGreggy@gmail.com'
+},
+{
+    id:2,
+    firstName: 'Scraggs',
+    lastName: 'Monet',
+    phoneNumber: '701-701-7011',
+    email: 'ScraggsMonet@gmail.com'
+}
+]
